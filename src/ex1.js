58196265b8ad3fcb27a1aa7746e6492737f97ae2
Francis Chung <francis.c.chung@gmail.com>
2025-07-08 15:50:34 +1000

    Output redirection lession

--- src/ex1.js
+++ src/ex1.js
@@ -1,4 +1,10 @@
 // My first Node.js program
-process.stdout.write("Hello Standout")
-console.log("");
+//process.stdout.write("Hello Standout")
+//console.log("");
 console.log("Hello World");
+console.error("Something went wrong");
+
+
+//node ex1.js 1>/dev/null
+//node ex1.js 2>/dev/null
+//node ex1.js 2>/dev/null 1>&2
