cc2d3275648420032fb285df440144fed7a936ef
Francis Chung <francis.c.chung@gmail.com>
2025-07-08 16:47:34 +1000

    printHelp lesson

---
+++ src/ex3.js
@@ -0,0 +1,13 @@
+#! /usr/bin/env node
+
+"use strict";
+
+printHelp();
+
+function printHelp() {
+    console.log("ex3 usuage:");
+    console.log("  ex3.js --help");
+    console.log("");
+    console.log("--help         print this help");
+    console.log("");
+}
