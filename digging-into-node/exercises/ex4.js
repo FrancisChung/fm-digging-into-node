33f569fef713b81ec9ab61a639a766818661e817
Francis Chung <francis.c.chung@gmail.com>
2025-08-09 14:33:30 +1000

    Rollback to original version

--- digging-into-node/exercises/ex4.js
+++ digging-into-node/exercises/ex4.js
@@ -16,7 +16,7 @@ const DB_PATH = path.join(__dirname,"my.db");
 const DB_SQL_PATH = path.join(__dirname,"mydb.sql");
 
 var args = require("minimist")(process.argv.slice(2),{
-	string: ["other",],
+    string: ["other",],
 });
 
 main().catch(console.error);
@@ -27,93 +27,44 @@ main().catch(console.error);
 var SQL3;
 
 async function main() {
-	if (!args.other) {
-		error("Missing '--other=..'");
-		return;
-	}
-
-	// define some SQLite3 database helpers
-	var myDB = new sqlite3.Database(DB_PATH);
-	SQL3 = {
-		run(...args) {
-			return new Promise(function c(resolve,reject){
-				myDB.run(...args,function onResult(err){
-					if (err) reject(err);
-					else resolve(this);
-				});
-			});
-		},
-		get: util.promisify(myDB.get.bind(myDB)),
-		all: util.promisify(myDB.all.bind(myDB)),
-		exec: util.promisify(myDB.exec.bind(myDB)),
-	};
-
-	var initSQL = fs.readFileSync(DB_SQL_PATH,"utf-8");
-    await SQ3L.exec(initSQL);
-
-
-	var other = args.other;
-	var something = Math.trunc(Math.random() * 1E9);
-
-	// ***********
-    var otherId = await insertOrLookupOther(other);
-    if (otherId) {
-        let result = await insertSomething(otherId, something);
-        if (result) {
-            return;
-        }
+    if (!args.other) {
+        error("Missing '--other=..'");
+        return;
     }
 
-	error("Oops!");
+    // define some SQLite3 database helpers
+    var myDB = new sqlite3.Database(DB_PATH);
+    SQL3 = {
+        run(...args) {
+            return new Promise(function c(resolve,reject){
+                myDB.run(...args,function onResult(err){
+                    if (err) reject(err);
+                    else resolve(this);
+                });
+            });
+        },
+        get: util.promisify(myDB.get.bind(myDB)),
+        all: util.promisify(myDB.all.bind(myDB)),
+        exec: util.promisify(myDB.exec.bind(myDB)),
+    };
+
+    var initSQL = fs.readFileSync(DB_SQL_PATH,"utf-8");
+    // TODO: initialize the DB structure
+
+
+    var other = args.other;
+    var something = Math.trunc(Math.random() * 1E9);
+
+    // ***********
+
+    // TODO: insert values and print all records
+
+    error("Oops!");
 }
 
 function error(err) {
-	if (err) {
-		console.error(err.toString());
-		console.log("");
-	}
-}
-
-async function insertOrLookupOther(other) {
-    var result = await SQL3.get(
-        `SELECT
-            id
-        FROM
-            Other
-        WHERE
-            data = ?
-        `,
-        other
-    );
-
-    if (result && result.id) {
-        return result.id;
+    if (err) {
+        console.error(err.toString());
+        console.log("");
     }
-    else {
-        result = await SQL3.run(`
-            INSERT INTO
-                Other (data)
-            VALUES
-                ?
-            `
-        );
-        if (result && result.lastID) {
-            return result.lastID;
-        }
-    }
-
-}
-
-async function insertSomething(otherId, something) {
-    var result = await SQL3.run(`
-            INSERT INTO
-                Other (data)
-            VALUES
-                ?
-            where id = (otherId)
-            `
-    );
-
-    if (result)
-        return result;
 }
