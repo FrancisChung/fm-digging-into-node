26b379ad062cfdc67afd380859aa863b62a13ec1
Francis Chung <francis.c.chung@gmail.com>
2025-10-18 17:42:37 +1100

    Rolling back to original version

--- digging-into-node/exercises/ex7-child.js
+++ digging-into-node/exercises/ex7-child.js
@@ -14,8 +14,5 @@ main().catch(() => 1);
 // ************************************
 
 async function main() {
-    var x = 0
-	for (let i = 0; i < 10000000000; i++) {
-        x = i + 1;
-    }
+    // To Do
 }
